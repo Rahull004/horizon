7c30be78cbd03ea6f2ca04794858265e414841c5
Rahul Bansal <rahulbansal24072004@gmail.com>
2024-05-16 19:17:52 +0530

    Add transactions on home page

--- app/(root)/transaction-history/page.tsx
+++ app/(root)/transaction-history/page.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import HeaderBox from "@/components/HeaderBox";
+import React from "react";
 
 const TransactionHistory = () => {
   return (
-    <div>TransactionHistory</div>
-  )
-}
+    <section className="transactions">
+      <div className="transaction-header">
+        <HeaderBox
+          title="Transaction History"
+          subtext="See your bank details and transactions"
+        />
+      </div>
+    </section>
+  );
+};
 
-export default TransactionHistory
+export default TransactionHistory;
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -6,6 +6,7 @@ import { sidebarLinks } from "@/constants";
 import { usePathname } from "next/navigation";
 import { cn } from "@/lib/utils";
 import Footer from "./Footer";
+import PlaidLink from "./PlaidLink";
 
 const Sidebar = ({ user }: SiderbarProps) => {
   const pathname = usePathname();
@@ -45,8 +46,9 @@ const Sidebar = ({ user }: SiderbarProps) => {
             </Link>
           );
         })}
+        <PlaidLink user={user} />
       </nav>
-      <Footer user={user} />
+      <Footer user={user} type={"mobile"} />
     </section>
   );
 };
